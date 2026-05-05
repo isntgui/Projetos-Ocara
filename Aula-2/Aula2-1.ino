23ebc462ad432c0bcc6895140082d67175e76b1f
agui <167501331+isntgui@users.noreply.github.com>
2026-05-05 15:47:55 -0400

    Update Aula2-1.ino

--- Aula-2/Aula2-1.ino
+++ Aula-2/Aula2-1.ino
@@ -4,19 +4,19 @@
 #define led_green 7
 #define led_blue 4
 
-int comb[][3] = {{0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {1, 1, 0}, {1, 0, 1}, {0, 1, 1}, {1, 1, 1}};
-int row = sizeof(comb) / sizeof(comb[0]);
+int comb[][3] = {{0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {1, 1, 0}, {1, 0, 1}, {0, 1, 1}, {1, 1, 1}}; // matriz para guardar todas as combinações possíveis entre os pinos
+int row = sizeof(comb) / sizeof(comb[0]); // sizeof lê o tamanho de alguma coisa (variavel e tipos) em bytes, estou utilizando para descobrir o número de linhas da matriz
 
 void setup() {
   pinMode(led_red, OUTPUT);
   pinMode(led_green, OUTPUT);
   pinMode(led_blue, OUTPUT);
 
-  Serial.begin(9600);
+  Serial.begin(9600); // Iniciar comunicação serial, configurando a taxa de transferência de dados (baud rate) em bits por segundo (usado para print, println e etc...)
 }
 
 void loop() {
-  for (int i = 0; i < row; i++) {
+  for (int i = 0; i < row; i++) { // estou percorrendo todas as combinações que estão armazenadas na matriz
     digitalWrite(led_red,   comb[i][0]);
     digitalWrite(led_green, comb[i][1]);
     digitalWrite(led_blue,  comb[i][2]);
