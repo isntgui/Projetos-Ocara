00986c004a292493b6806da19302cf1274b098c8
agui <167501331+isntgui@users.noreply.github.com>
2026-05-05 14:30:36 -0400

    Update Aula1.ino

--- Aula-1/Aula1.ino
+++ Aula-1/Aula1.ino
@@ -3,12 +3,12 @@
 #define led_vermelho 10
 
 void setup() {
-  pinMode(led_vermelho, OUTPUT);
+  pinMode(led_vermelho, OUTPUT);        // led é uma saída de dados, então é output
 }
 
 void loop() {
-  digitalWrite(led_vermelho, HIGH);
-  delay(1000);
-  digitalWrite(led_vermelho, LOW);
-  delay(1000);
+  digitalWrite(led_vermelho, HIGH);    // ascender o led utilizando o HIGH
+  delay(1000);                         // espera de um segundo (1000 ms) com o led ligado
+  digitalWrite(led_vermelho, LOW);     // desligar um led utilizando o LOW
+  delay(1000);                         // espera de um segundo (1000 ms) com o led desligado
 }
