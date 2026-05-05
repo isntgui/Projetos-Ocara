d54473432c03e520d7a6dd4ae77dff5abc7f6b31
agui <167501331+isntgui@users.noreply.github.com>
2026-05-05 15:42:07 -0400

    Add RGB LED control code in Aula2-1.ino
    Implement RGB LED control to cycle through color combinations.

---
+++ Aula-2/Aula2-1.ino
@@ -0,0 +1,27 @@
+// código para ascender todas as cores com RGB (forma minimizada sem muita repetição de código)
+
+#define led_red 10
+#define led_green 7
+#define led_blue 4
+
+#define ascender digitalWrite
+
+int comb[][3] = {{0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {1, 1, 0}, {1, 0, 1}, {0, 1, 1}, {1, 1, 1}};
+int row = sizeof(comb) / sizeof(comb[0]);
+
+void setup() {
+  pinMode(led_red, OUTPUT);
+  pinMode(led_green, OUTPUT);
+  pinMode(led_blue, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int i = 0; i < row; i++) {
+    ascender(led_red,   comb[i][0]);
+    ascender(led_green, comb[i][1]);
+    ascender(led_blue,  comb[i][2]);
+    delay(1000);
+  }
+}
