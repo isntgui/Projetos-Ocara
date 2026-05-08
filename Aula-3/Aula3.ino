56ecd902a2f677d36f60b6c2f46696adeb7a771d
agui <167501331+isntgui@users.noreply.github.com>
2026-05-08 12:45:09 -0400

    Add 7-segment display counting from 0 to 9
    This code initializes a 7-segment display to count from 0 to 9, setting the appropriate pins as outputs and updating the display in a loop.

---
+++ Aula-3/Aula3.ino
@@ -0,0 +1,27 @@
+// ascender o led de 7 segmentos e contar de 0 até 9...
+const byte digitos[10][7] = {{1, 1, 1, 0, 1, 1, 1},
+							               {1, 0, 0, 0, 1, 0, 0},
+              							 {1, 1, 0, 1, 0, 1, 1},
+              							 {1, 1, 0, 1, 1, 1, 0},
+              							 {1, 0, 1, 1, 1, 0, 0}, 
+              							 {0, 1, 1, 1, 1, 1, 0},
+              							 {0, 1, 1, 1, 1, 1, 1},
+               							 {1, 1, 0, 0, 1, 0, 0},
+              							 {1, 1, 1, 1, 1, 1, 1},
+              							 {1, 1, 1, 1, 1, 1, 0}};
+    					            // {b, a, f, g, c, d, e}
+
+void setup(){
+  for(int pino = 13; pino >= 7; pino--){
+    pinMode(pino, OUTPUT);
+  }
+}
+
+void loop(){
+  for (int digit = 0; digit < 10; digit++) { // [0, 9]
+    for (int pino = 13; pino >= 7; pino--) {
+      digitalWrite(pino, digitos[digit][13 - pino]);
+    }
+    delay(1000);
+  }
+}
