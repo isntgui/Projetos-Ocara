7f2d25ca5bff8c4619c1de38d29ed68b6a4eff74
agui <167501331+isntgui@users.noreply.github.com>
2026-06-09 14:01:55 -0400

    Create Aula9.ino

---
+++ Aula-9/Aula9.ino
@@ -0,0 +1,90 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <DHT.h>
+
+#define DHTPIN 2       // pino do DHT11
+#define DHTTYPE DHT11
+#define LEDPIN 4       // pino do LED
+
+const char* ssid = "I4";
+const char* password = "I4emssocara@nov2023";
+
+ESP8266WebServer server(80);
+DHT dht(DHTPIN, DHTTYPE);
+
+bool ledState = false;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, LOW);
+
+  dht.begin();
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Conectando");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");
+  Serial.println(WiFi.localIP());
+
+  // rota para enviar dados
+  server.on("/get", handleData);
+
+  // rota para controlar LED
+  server.on("/led", handleLED);
+
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handleData() {
+
+  float temperatura = dht.readTemperature();
+  float umidade = dht.readHumidity();
+
+  if (isnan(temperatura) || isnan(umidade)) {
+    server.send(200, "application/json", "{\"erro\":\"falha sensor\"}");
+    return;
+  }
+
+  String json = "{";
+  json += "\"temperatura\":" + String(temperatura) + ",";
+  json += "\"umidade\":" + String(umidade) + ",";
+  json += "\"led\":" + String(ledState ? 1 : 0);
+  json += "}";
+
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "application/json", json);
+}
+
+void handleLED() {
+
+  if (server.hasArg("valor")) {
+
+    String valor = server.arg("valor");
+
+    if (valor == "1") {
+      digitalWrite(LEDPIN, HIGH);
+      ledState = true;
+    } 
+    else {
+      digitalWrite(LEDPIN, LOW);
+      ledState = false;
+    }
+  }
+
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "text/plain", "OK");
+}
